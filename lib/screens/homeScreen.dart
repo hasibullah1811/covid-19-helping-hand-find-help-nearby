73c653d29d5790646d7618d9467bc2c78fee8494
Hasib Ullah <hasib.ascend@gmail.com>
2020-04-15 21:08:47 +0600

    google sign out added to homescreen for testing

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -22,6 +25,14 @@ class _HomeScreenState extends State<HomeScreen> {
             RaisedButton(
               onPressed: () {
                 FirebaseAuth.instance.signOut();
+              },
+            ),
+            Container(
+              child: Text("Google Logout"),
+            ),
+            RaisedButton(
+              onPressed: () {
+                googleSignIn.signOut();
               },
             ),
           ],
