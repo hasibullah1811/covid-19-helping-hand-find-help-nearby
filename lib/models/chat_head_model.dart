344019886c13cc2d717eed254f631e5aa8172788
Ahmed Afridee <a.afridee47@gmail.com>
2020-04-25 15:58:45 +0600

    Messaging updated

--- lib/models/chat_head_model.dart
+++ lib/models/chat_head_model.dart
@@ -28,6 +28,7 @@ class buildChatHeadsState extends State<buildChatHeads> {
 
   String otherPersonphotUrl = 'https://firebasestorage.googleapis.com/v0/b/helping-hand-76970.appspot.com/o/default-user-img.png?alt=media&token=d96df74f-5b3b-4f08-86f8-d1a913459e07';
   String otherPersonName = 'loading..';
+  String otherPersonID = 'loading..';
   String lastMessage = 'loading..';
   String lastMessageSender = 'loading..';
 
@@ -39,6 +40,7 @@ class buildChatHeadsState extends State<buildChatHeads> {
          if(otherPerson.data['id']!=widget.me){
            setState(() {
              otherPersonName = otherPerson.data['name'];
+             otherPersonID = otherPerson.data['id'];
              if(otherPerson.data['photUrl']!="" && otherPerson.data['photUrl']!=null){
                otherPersonphotUrl = otherPerson.data['photUrl'];
              }else{
@@ -80,21 +82,9 @@ class buildChatHeadsState extends State<buildChatHeads> {
         padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
         child: InkWell(
             onTap: () async{
-              final CollectionReference get_post = Firestore.instance.collection('helpRequests/${widget.postOwnerID}_${widget.postID}/userPost');
-
-              Map<String, dynamic> post_info;
-              await for(var snapshot in get_post.snapshots()){
-                for(var post in snapshot.documents){
-                  setState(() {
-                    post_info = post.data;
-                  });
-                }
-                break;
-              }
-
               var route = new MaterialPageRoute(
                   builder: (BuildContext context) =>
-                  new ChatScreen(theOtherPerson:User( id: post_info['ownerID'],imageUrl: post_info['photUrl'] ,name: post_info ['name']), messageField: 'messages/${widget.helperID}_${widget.postID}',)
+                  new ChatScreen(theOtherPerson:User( id: otherPersonID,imageUrl: otherPersonphotUrl,name: otherPersonName), messageField: 'messages/${widget.helperID}_${widget.postID}',)
               );
               Navigator.of(context).push(route);
             },
